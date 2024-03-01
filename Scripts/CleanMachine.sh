db723ffef82e9210fcdf60e096f020c7089d8ef6
George E-Green <112101154+Athen-Player1@users.noreply.github.com>
2024-03-01 12:00:11 +0000

    tidying, making readable adding ssh

--- Scripts/CleanMachine.sh
+++ Scripts/CleanMachine.sh
@@ -15,20 +15,14 @@ print_red() {
     echo -e "${RED}$1${RESET}"
 }
 
-# Prompt the user
-read -p "This script will clean the machine. It will remove the AWS CLI and creds, the bash history, any logged-in GitHub accounts, and perform apt autoremove. 
-are you sure you would like to continue" response 
-# Check the response
-if [[ $response =~ ^[Yy]$ ]]; then
-    # Run the clean up for AWS and github Creds
-    rm ~/.aws/credentials
+cleanAWS(){
+ rm ~/.aws/credentials
     print_green "----------AWS creds removed----------"
+}
 
-    rm ~/.git-credentials
-    print_green "----------GitHub creds removed----------"
-
-    # Double check its okay to remove chrome data
-    read -p "Remove chrome logins, history and cookies (Y/N)" response2
+cleanChrome(){
+     # Double check its okay to remove contents of downloads
+     read -p "Remove chrome logins, history and cookies (Y/N)" response2
     if [[ $response2 =~ ^[Yy]$ ]]; then
         rm -rf ~/.cache/google-chrome/*
         rm -f ~/.config/google-chrome/Default/*
@@ -36,8 +30,10 @@ if [[ $response =~ ^[Yy]$ ]]; then
     else 
          print_red "----------Chrome Skipped----------"
     fi
+}
 
-    # Double check its okay to remove contents of downloads
+cleanDownloads(){
+     # Double check its okay to remove contents of downloads
     read -p "Remove contents of downloads folder (Y/N)" response3
     if [[ $response3 =~ ^[Yy]$ ]]; then
         rm -r ~/Downloads/*
@@ -45,15 +41,46 @@ if [[ $response =~ ^[Yy]$ ]]; then
     else 
          print_red "----------Downloads Skipped----------"
     fi
-    
-    print_green "----------Clearing Bash History----------"
+}
+
+cleanHistory(){
     history -c
     history -w
     print_green "----------Bash History Cleared----------"
 
+}
+
+cleanGit(){
     sudo apt clean
     sudo apt autoremove
     print_green "----------Cleaning Complete----------"
+}
+
+cleanSSH(){
+    # Double check its okay to remove all ssh keys
+    read -p "Remove all ssh keys from ~/.ssh (Y/N)" response4
+    if [[ $response4 =~ ^[Yy]$ ]]; then
+        rm -r ~/.ssh/*
+        print_green "----------SSH Keys Cleared----------"
+    else 
+         print_red "----------SSH Skipped----------"
+    fi
+}
+
+# Prompt the user
+read -p "This script will clean the machine. It will remove the AWS CLI and creds, the bash history, any logged-in GitHub accounts, and perform apt autoremove. 
+are you sure you would like to continue" response 
+# Check the response
+if [[ $response =~ ^[Yy]$ ]]; then
+    # Run the clean up for AWS and github Creds
+   
+    cleanAWS
+    cleanGit
+    cleanChrome
+    cleanDownloads
+    cleanHistory
+    cleanSSH
+
 
     # Kill the script if no is selected
 elif [[ $response =~ ^[Nn]$ ]]; then
