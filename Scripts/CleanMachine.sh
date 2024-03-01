dffed39e49a38161231b338a0c1cb85f9a5bc048
George E-Green <112101154+Athen-Player1@users.noreply.github.com>
2024-03-01 14:16:16 +0000

    clean_git cleanup

--- Scripts/CleanMachine.sh
+++ Scripts/CleanMachine.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+
 # Define color escape codes
 GREEN='\033[0;32m'
 RED='\033[0;31m'
@@ -24,7 +25,7 @@ print_yellow() {
 # check whether user had supplied -h or --help
 if [[ ( $@ == "--help") ||  $@ == "-h" ]]
 then 
-	echo "A simple program to clean a Server PC before delivery. Please run with SUDO to ensure sanitisation"
+	echo "Please run with SUDO to ensure sanitisation"
 	exit 0
 fi 
 
@@ -89,8 +90,9 @@ clean_history(){
 clean_git(){
     # Double check its okay to remove all git creds
     if confirm "Clear GitHub creds (Y/N) "; then
-        sudo apt clean
-        sudo apt autoremove
+        git config --global --unset credential.helper
+        git credential-cache exit
+        rm ~/.git-credentials
         print_green "----------Git creds cleared----------"
     else 
          print_red "----------Git Creds Skipped----------"
@@ -107,23 +109,36 @@ clean_ssh(){
     fi
 }
 
+clean_cache(){
+    if confirm "Would you like to remove the system cache? /tmp /var/tmp /var/lib/apt/lists/ ~/.cache (Y/N) "; then
+        sudo apt clean
+        sudo apt autoremove
+        rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /root/.cache
+        print_green "-----------Cleared Cache-----------"
+    else
+        print_red "-----------Cache Skipped----------"
+    fi
+
+}
+
 # Prompt for script to run
 print_yellow "----------Delievery NUC Cleaner----------"
 if confirm "This script will clean the machine. It will remove the AWS CLI and creds, the bash history, any logged-in GitHub accounts, and perform apt autoremove. 
 are you sure you would like to continue (Y/N) " ; then
     # Run the clean up the process print the current step
-    print_yellow "Step 1/6"
+    print_yellow "Step 1/7"
     clean_aws
-    print_yellow "Step 2/6"
-    clean_git
-    print_yellow "Step 3/6"
+    print_yellow "Step 2/7"
     clean_chrome
-    print_yellow "Step 4/6"
+    print_yellow "Step 3/7"
     clean_downloads
-    print_yellow "Step 5/6"
+    print_yellow "Step 4/7"
     clean_history
-    print_yellow "Step 6/6"
+    print_yellow "Step 5/7"
+    clean_git
+    print_yellow "Step 6/7"
     clean_ssh
+    print_yellow "Step 7/7
     print_yellow "----------Cleaning Complete----------"
 
     # Kill the script if no is selected
