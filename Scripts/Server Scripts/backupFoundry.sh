63f336cdedf4c2cd676a94739e39f23c49c9c325
Athen-Player1 <georgee-green@outlook.com>
2025-01-29 13:29:43 +0000

    refactor foundrybackup

--- Scripts/Server Scripts/backupFoundry.sh
+++ Scripts/Server Scripts/backupFoundry.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
-foundrypath= "/home/jumble/foundry"
-backuppath= "/home/jumble/foundrybackup"
-remotepath= "/mnt/gdrive/foundrybackup"
-discordurl= "https://discord.com/api/webhooks/1234567890/abcdefghijklmnopqrstuvwxyz"
+foundrypath="/home/jumble/foundry/"
+backuppath="/home/jumble/foundrybackup/"
+remotepath="/mnt/gdrive/foundrybackup/"
+discordurl="https://discord.com/api/webhooks/99999/wbc123"
 
-tar -czvf $backuppath-$(date +%Y%m%d).tar.gz $foundrypath
-mv $backuppath-$(date +%Y%m%d).tar.gz $remotepath-$(date +%Y%m%d).tar.gz
+#tar foundry directory and post to discord
+tarfoundry(){
+tar -czvf ${backuppath}backup_$(date +%Y%m%d).tar.gz $foundrypath
+if [ $? -ne 0 ]; then
+    curl -H "Content-Type: application/json" -X POST -d '{"content":"Foundry Backup Failed"}' $discordurl
+    exit 1
+else 
+    curl -H "Content-Type: application/json" -X POST -d '{"content":"Foundry Tar Complete"}' $discordurl
+fi
+}
+
+movefoundry(){
+#move tar to remote path and post to discord
+mv ${backuppath}backup_$(date +%Y%m%d).tar.gz ${remotepath}backup_$(date +%Y%m%d).tar.gz
+if [ $? -ne 0 ]; then
+    curl -H "Content-Type: application/json" -X POST -d '{"content":"Foundry Backup Failed"}' $discordurl
+    exit 1
+else 
+    curl -H "Content-Type: application/json" -X POST -d '{"content":"Foundry Move Complete"}' $discordurl
+fi
+}
+#prune backups
+prunebackups(){
+find ${remotepath} -type f -mtime +30 -exec rm {} \;
+}
+
+#run functions
+tarfoundry
+movefoundry
+prunebackups
 
 #send discord message
-curl -H "Content-Type: application/json" -X POST -d '{"content":"Foundry Backup Complete"}' https://discord.com/api/webhooks/1234567890/abcdefghijklmnopqrstuvwxyz
-
-#send discord message on failure
-if [ $? -ne 0 ]; then
-  curl -H "Content-Type: application/json" -X POST -d '{"content":"Foundry Backup Failed"}' https://discord.com/api/webhooks/1234567890/abcdefghijklmnopqrstuvwxyz
-fi
+curl -H "Content-Type: application/json" -X POST -d '{"content":"Foundry Backup Complete"}' $discordurl
